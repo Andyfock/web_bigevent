b5350f9e143eb28e0fda9afaaa4369a39064ccb7
Yufeng Jiang <279640062@qq.com>
2021-12-21 15:11:03 +0800

    init project

---
+++ assets/js/login.js
@@ -0,0 +1,36 @@
+//入口函数
+//为登陆和注册的div里的a链接各自绑定一个点击事件来控制注册和登陆链接的显示
+$(function() {
+    //点击去注册账号的链接
+    $('#link_reg').on('click', function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    //点击去登陆的链接
+    $('#link_login').on('click', function() {
+        $('.reg-box').hide()
+        $('.login-box').show()        
+    })
+
+    //从Layui获取到一个form对象
+    var form = layui.form
+    //通过form.verify函数来自定义校验规则
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+        ],
+        //校验两次密码是否一致
+        repwd: function(value) {
+            //通过形参拿到的是确认密码框中的内容
+            //还需要拿到密码框中的内容
+            //然后进行一次等于的判断
+            //如果判断失败return一个提示消息
+            var pwd = $('.reg-box [name=password]').val()
+            if(pwd !== value) {
+                return '两次密码不一致！'
+            }
+        } 
+    })
+})
