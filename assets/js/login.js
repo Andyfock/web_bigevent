2d210b35b20aa1291f38dc6e3ba84deba2239f4d
Yufeng Jiang <279640062@qq.com>
2021-12-21 22:14:49 +0800

    register API

--- assets/js/login.js
+++ assets/js/login.js
@@ -35,4 +35,13 @@ $(function() {
     })
 
     //监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e){
+        e.preventDefault()
+        $.post('http://api-breakingnews-web.itheima.net/api/reguser', {username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val()}, function (res) {
+            if(res.status !== 0){
+                return console.log(res.message);
+            }
+            console.log('register success!');
+        })
+    })
 })
